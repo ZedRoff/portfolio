517b445523809b61da0527829eeb9a6566ede955
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-27 20:23:50 +0200

    Navbar

--- src/general/header.js
+++ src/general/header.js
@@ -1,7 +1,11 @@
-import React from "react"
-
+import {React, useState} from "react"
+import { Link} from "react-router-dom"
 function Header() {
+
+    const [status, setStatus] = useState(false)
+console.log(window.screen.width)
     return(
+        <>
         <header>
             <div className="child_header">
                 <h2 className="child_header_title">
@@ -9,15 +13,25 @@ function Header() {
                     </h2>
             </div>
             <ul className="child_header">
-                <li className="child_header_element">Home</li>
-                <li className="child_header_element">Projects</li>
-                <li className="child_header_element">Me</li>
-                <li className="child_header_element">Links</li>
-                <li className="child_header_element">Blog</li>
-                <li className="child_header_element">Contact</li>
+             <Link to="/">   <li className="child_header_element">Home</li></Link>
+             <Link to="/projects">  <li className="child_header_element">Projects</li></Link>
+             <Link to="/me">  <li className="child_header_element">Me</li></Link>
+             <Link to="/links">  <li className="child_header_element">Links</li></Link>
+             <Link to="/blog">  <li className="child_header_element">Blog</li></Link>
+             <Link to="/contact"> <li className="child_header_element">Contact</li></Link>
             </ul>
-
+            <i className="fas fa-bars" onClick={() => setStatus(!status)}></i>
         </header>
+        {(status) ? <nav class="deployer">
+        <Link to="/" className="deployer_element">   <li >Home</li></Link>
+             <Link to="/projects" className="deployer_element">  <li >Projects</li></Link>
+             <Link to="/me" className="deployer_element">  <li>Me</li></Link>
+             <Link to="/links" className="deployer_element">  <li >Links</li></Link>
+             <Link to="/blog" className="deployer_element">  <li >Blog</li></Link>
+             <Link to="/contact" className="deployer_element"> <li >Contact</li></Link>
+        </nav> : "" }
+       
+        </>
     )
 }
 
