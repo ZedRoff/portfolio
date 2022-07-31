9b2217af5ef2c3c7b93e7f4cf26d3bcb0f8ad248
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-08-01 00:19:47 +0200

    init back end

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+let PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log("backend prêt")
+})
+app.get("/api/test", (req, res) => {
+    return res.send("404 mdr")
+})
