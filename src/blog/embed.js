0d27122139e7e01bdc071c7c68b6fbda2359d3eb
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-31 23:49:33 +0200

    embed struct

--- src/blog/embed.js
+++ src/blog/embed.js
@@ -2,8 +2,8 @@
 function Embed(props) {
   return (
 <div className="embed_blog">
-    <h2 clasName="embed_blog_title">{props.title}</h2>
-    <p clasName="embed_blog_description">{props.description}</p>
+    <h2 className="embed_blog_title">{props.title}</h2>
+    <p className="embed_blog_description">{props.description}</p>
     <span className="embed_blog_footer">{props.footer}</span>
     </div>
   );
--- src/blog/main.js
+++ src/blog/main.js
@@ -1,9 +1,10 @@
 import Title from "../general/title";
+import Embed from "./embed";
 function Main() {
   return (
 <main className="blog_page">
 <Title name="Blog" />
-
+<Embed title="Why we code ?" description="An article to encourage peoples starting coding :D" footer="2020/7/2 | Redacted By ZedRoff | Owner." />
    </main>
   );
 }
