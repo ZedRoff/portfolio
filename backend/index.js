d5d5e8888db8123712f8901267a41ae550f1117d
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-08-01 21:12:01 +0200

    test

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-let PORT = process.env.PORT || 5000
+let PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log("backend prêt")
 })
