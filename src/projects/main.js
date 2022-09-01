4b1c66c9d14ba43c57701da523f84ab64620bf0d
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-09-01 20:54:51 +0200

    projects change

--- src/projects/main.js
+++ src/projects/main.js
@@ -8,16 +8,16 @@ function Main() {
  <div className="language_name"><h2 className="language_name_content">JavaScript Projects</h2></div>
 
  <div className="container_cards">
-    <Card name="Example Bot" description="
-            Example-bot is a multipurpose bot only for an educational purpose on github
-          " image="https://cdn.discordapp.com/avatars/489417394960203777/1856e241e7a175a93651976ad2bff14d.png?size=2048"/>
+    <Card name="Camo Bot" description="
+            A multipurpose Discord Bot developed with JS runtime NodeJS and DiscordJS.
+          " image="https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/1081073/retina_1708x683_cover-how-to-make-a-discord-bot-7c0fe302b98b05b145682344b3a4ec59.png"/>
     <Card name="
             TheAPI
           " description="
-          A Rest API coded in NodeJS to fetch informations in game.
+          A Rest API with an interface, using EJS, NodeJS, and expressJS.
          " image="https://cdn.discordapp.com/icons/587220309065596944/833681e9d243d515abe93a3aed82fb13.webp?size=2048"/>
-    <Card name="Ahov" description="
-            An electron build app, that gives you the informations that you need at the time ! Under Development ! " image="https://cdn.discordapp.com/icons/692762301329309697/92da45f46503b5ef1d8ce0c24e096735.webp?size=128"/>
+    <Card name="Chat-App" description="
+           A chat app built with sockets and basic NodeJS" image="https://cdn.discordapp.com/icons/692762301329309697/92da45f46503b5ef1d8ce0c24e096735.webp?size=128"/>
  </div>
 
  <div className="language_name"><h2 className="language_name_content">HTML/CSS Projects</h2></div>
@@ -27,10 +27,26 @@ function Main() {
     <Card name="
             EzDevs
           " description="A website to share some tips, and to add paste and share them with other peoples." image="https://cdn.glitch.com/533e1033-6025-49f4-b312-dfe999f8a402%2Ft%C3%A9l%C3%A9chargement.jpg?v=1594920255687"/>
-    <Card name="Twisted Felow" description="Only a template now, it's the first website that I built." image="https://cdn.glitch.com/f109d061-b5c1-49ce-be13-3b1e8d14fc9c%2F01F7179D-BEE2-4F16-8D9A-733BEC694E24.jpeg?v=15626887"/>
+    <Card name="Objective All Inclusive" description="A website built to convert gender oriented texts to neutral ones" image="https://cdn.glitch.com/f109d061-b5c1-49ce-be13-3b1e8d14fc9c%2F01F7179D-BEE2-4F16-8D9A-733BEC694E24.jpeg?v=15626887"/>
  </div>
+ <div className="language_name"><h2 className="language_name_content">Python Projects</h2></div>
 
+<div className="container_cards">
+   <Card name="Tic tac toe" description="Basic tic tac toe built for a school project using Tkinter" image="https://cdn.glitch.com/533e1033-6025-49f4-b312-dfe999f8a402%2FThe-Evolution-Of-Web-Development-Via-Machine-Learning.jpg?v=1594919839072"/>
+   <Card name="
+           HangMan
+         " description="Hangman game that can be played using python's console, for a Discord Challenge" image="https://cdn.glitch.com/533e1033-6025-49f4-b312-dfe999f8a402%2Ft%C3%A9l%C3%A9chargement.jpg?v=1594920255687"/>
 
+</div>
+<div className="language_name"><h2 className="language_name_content">Java Projects</h2></div>
+
+<div className="container_cards">
+   <Card name="Coming soon ..." description="Since I am now a student at ESIEE Paris, our main language " image="https://cdn.glitch.com/533e1033-6025-49f4-b312-dfe999f8a402%2FThe-Evolution-Of-Web-Development-Via-Machine-Learning.jpg?v=1594919839072"/>
+   <Card name="
+           HangMan
+         " description="Hangman game that can be played using python's console, for a Discord Challenge" image="https://cdn.glitch.com/533e1033-6025-49f4-b312-dfe999f8a402%2Ft%C3%A9l%C3%A9chargement.jpg?v=1594920255687"/>
+
+</div>
    </>
   );
 }
