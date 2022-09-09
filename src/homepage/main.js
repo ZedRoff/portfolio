e24cc9fc23da9e49507127977340c87645c415ae
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-09-09 14:22:50 +0200

    add effect on university name on homepage + hover effect on it

--- src/homepage/main.js
+++ src/homepage/main.js
@@ -5,7 +5,7 @@ function Main() {
         <main className="homepage_main">
 <img src="https://cdn.glitch.com/04a4d0d3-5530-4593-8c2b-59b2a71007bf%2F3f18167d483a335895c6bad29836a76a.png?v=1587400343320" className="image_main" alt="no source" />
            
-           <h2 className="main_title">Hello ! I'm ZedRoff, student at ESIEE Paris<br />Welcome in my portfolio</h2>
+           <h2 className="main_title">Hello ! I'm ZedRoff, student at <a href="https://www.esiee.fr/" target="_blank" rel="norefferer" id="univ">ESIEE Paris</a><br />Welcome in my portfolio</h2>
         
             </main>
     )
