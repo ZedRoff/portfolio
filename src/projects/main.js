1b57fd9364bc97e4f2e5a25b5561c7b6247ef097
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-09-09 16:24:24 +0200

    removed template from projects page

--- src/projects/main.js
+++ src/projects/main.js
@@ -41,10 +41,5 @@ Object.keys(json).map((key, index) => {
    </>
   );
 }
-/*<div className="language_name"><h2 className="language_name_content">Java Projects</h2></div>
 
-<div className="container_cards">
-   <Card name="Coming soon ..." description="Since I am now a student at ESIEE Paris, our main language " image="https://cdn.glitch.com/533e1033-6025-49f4-b312-dfe999f8a402%2FThe-Evolution-Of-Web-Development-Via-Machine-Learning.jpg?v=1594919839072" url="https://github.com/zedroff" />
-
-</div>*/
 export default Main;
